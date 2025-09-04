bd569a235a84f330961beee569c7965e6f2b87dd
Wz-00 <wildanjk14@gmail.com>
2025-09-04 21:58:58 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue_wz',
+  webDir: 'dist'
+};
+
+export default config;
